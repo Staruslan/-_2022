370ce5020a576bd0e8ea024f047d3e18aef245f7
RUSLAN <67726937+Staruslan@users.noreply.github.com>
2022-03-13 21:16:25 +0800

    Add files via upload

---
+++ HomeWork3.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using namespace std ;
+
+
+int main () {
+//#1
+  int firstNum, secondNum;
+
+  printf("Please, input A:\n");
+  scanf("%d", &firstNum);
+
+  printf("Input B:       ( B > A !!!)\n");
+  scanf("%d", &secondNum);
+
+  if ( firstNum > secondNum ) {
+
+    printf("Condition not met : %d > %d \n", firstNum, secondNum);
+    return 0;
+
+  }
+
+  printf("Writing to 'Output'....\n");
+
+  FILE *fileToWrite;
+  fileToWrite = fopen("Output", "w");
+
+
+   while (1) {
+     if (firstNum > secondNum) {
+       break;
+     }
+     int valueToFile = firstNum * 3;
+     fprintf(fileToWrite, "%d\n", valueToFile);
+     firstNum++;
+   }
+
+   fclose(fileToWrite);
+//#2
+    printf("Reading from 'Output':\n");
+    int value;
+    int n = 1;
+    ifstream fileToRead("Output");
+
+    while (1) {
+      fileToRead >> value;
+
+      if (fileToRead.eof()) {
+        break;
+      }
+      cout << n << "." << value << "\n";
+      n++;
+
+    }
+
+    fileToRead.close();
+
+  return 0;
+
+
+
+}
