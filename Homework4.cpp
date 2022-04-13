1249bc82ff25dd14490746d3e502e89829ba4121
RUSLAN <67726937+Staruslan@users.noreply.github.com>
2022-04-13 21:44:50 +0800

    Update Homework4.cpp

--- Homework4.cpp
+++ Homework4.cpp
@@ -21,28 +21,4 @@ int main()
  return 0;
 }
 
-//Задание 4.2
-#include <iostream>
-#include <cstring>
-using namespace std;
 
-int main()
-{
-    char str[80];
-    cin >> str;
-            
-            int i = 0;
-            while (i < strlen(str))
-            {
-               int ascii = (int)str[i];
-                if (ascii == 122)
-                {
-                    ascii = (int)str[i] - 25; 
-                }
-                else ascii = (int)str[i] + 1;
-                cout << (char)ascii;
-                ++i;
-            }
-        char c;
-    return 0;
-}
