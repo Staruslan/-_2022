7ab1bb771443926103da877fba4afd877360db70
RUSLAN <67726937+Staruslan@users.noreply.github.com>
2022-04-13 22:39:54 +0800

    Update Homework4.cpp

--- Homework4.cpp
+++ Homework4.cpp
@@ -22,3 +22,21 @@ int main()
 }
 
 
+//Задание 4.2
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char s1[80];
+    cout << "Введите строку: ";
+    cin >> s1;//ввод строки с клавиатуры
+    cout <<"Вот ваша строка:" ;
+    for (int i = 0; i < strlen(s1); i++) {
+        cout <<char (s1[i] + 1);
+        
+    }
+    return 0;
+}
