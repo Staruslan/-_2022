343b37b2bd3d2bede3f3000b12e960f80955126d
RUSLAN <67726937+Staruslan@users.noreply.github.com>
2022-04-14 14:18:09 +0800

    Update Циклически перебрать.cpp

--- Циклически перебрать.cpp
+++ Циклически перебрать.cpp
@@ -6,6 +6,8 @@ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, S
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+//Задание 2.1
+
 #include <iostream>
  
 using namespace std;
@@ -15,5 +17,29 @@ int main()
     for(int i = 0; i <= 100; i++)
         if(i%3 == 0)
             cout << i << endl;
+    return 0;
+}
+
+//Задание 2.2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    int j = 0;
+    while (i < 1000)
+    {
+        cout << i << endl;    
+        if (i % 7 == 0) {
+           j += i;
+       }  
+
+       i += 4;
+     
+    }
+    cout << "Сумма всех чисел:" <<j << endl;
     return 0;
 }
